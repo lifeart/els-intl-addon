48485e07502bff52b755d81dc41be8381e20e4f1
Yuri Ratanov <organium@gmail.com>
2020-12-08 20:21:30 +0700

    add editor config and prettierrc

--- index.test.js
+++ index.test.js
@@ -1,24 +1,24 @@
-const { onComplete } = require("./index");
+const { onComplete } = require('./index');
 
-describe("empty autocomplete", () => {
-  it("should not autocomplete if no data", () => {
+describe('empty autocomplete', () => {
+  it('should not autocomplete if no data', () => {
     expect(
-      onComplete("", {
+      onComplete('', {
         results: [],
-        type: "template",
+        type: 'template',
         position: {
           character: 1,
           line: 1,
         },
         focusPath: {
           node: {
-            type: "StringLiteral",
-            value: "",
+            type: 'StringLiteral',
+            value: '',
           },
           parent: {
-            type: "MustacheStatement",
+            type: 'MustacheStatement',
             path: {
-              original: "t",
+              original: 't',
             },
           },
         },
@@ -26,36 +26,36 @@ describe("empty autocomplete", () => {
     ).toMatchSnapshot();
   });
 
-  describe("Itnl translations", () => {
-    it("should autocomplete root translation", () => {
+  describe('Itnl translations', () => {
+    it('should autocomplete root translation', () => {
       expect(
-        onComplete("./test-fixtures", {
+        onComplete('./test-fixtures', {
           results: [],
-          type: "template",
+          type: 'template',
           position: {
             character: 1,
             line: 1,
           },
           focusPath: {
             node: {
-              type: "StringLiteral",
-              value: "rootFileTransla",
+              type: 'StringLiteral',
+              value: 'rootFileTransla',
             },
             parent: {
-              type: "MustacheStatement",
+              type: 'MustacheStatement',
               path: {
-                original: "t",
+                original: 't',
               },
             },
           },
         })
       ).toEqual([
         {
-          detail: "en-us : text 1",
+          detail: 'en-us : text 1',
           kind: 0,
-          label: "rootFileTranslation",
+          label: 'rootFileTranslation',
           textEdit: {
-            newText: "rootFileTranslation",
+            newText: 'rootFileTranslation',
             range: {
               end: {
                 character: -13,
@@ -69,37 +69,37 @@ describe("empty autocomplete", () => {
           },
         },
       ]);
-    })
-    
-    it("should autocomplete sub folder translation", () => {
+    });
+
+    it('should autocomplete sub folder translation', () => {
       expect(
-        onComplete("./test-fixtures", {
+        onComplete('./test-fixtures', {
           results: [],
-          type: "template",
+          type: 'template',
           position: {
             character: 1,
             line: 1,
           },
           focusPath: {
             node: {
-              type: "StringLiteral",
-              value: "subFolderTranslat",
+              type: 'StringLiteral',
+              value: 'subFolderTranslat',
             },
             parent: {
-              type: "MustacheStatement",
+              type: 'MustacheStatement',
               path: {
-                original: "t",
+                original: 't',
               },
             },
           },
         })
       ).toEqual([
         {
-          detail: "en-us : text 2",
+          detail: 'en-us : text 2',
           kind: 0,
-          label: "subFolderTranslation.subTranslation",
+          label: 'subFolderTranslation.subTranslation',
           textEdit: {
-            newText: "subFolderTranslation.subTranslation",
+            newText: 'subFolderTranslation.subTranslation',
             range: {
               end: {
                 character: -15,
